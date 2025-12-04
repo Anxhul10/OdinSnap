995ab5c2d36598e52a66df0784e36f45fc9eedac
Anxhul10 <anxhul10@gmail.com>
2025-12-05 04:18:40 +0530

    fix: version conflicts yarn

--- cli/cli.ts
+++ cli/cli.ts
@@ -2,13 +2,18 @@
 import { checkPkgExist } from "../packages/cliHelpers/checkPkgExist.js";
 import { generateStats } from "../packages/cliHelpers/generateStats.js";
 import { mkdir } from "../packages/cliHelpers/mkdir.js";
+import { trimStats } from "../packages/utils/trimStats.js";
 
 const res = checkPkgExist("./package.json", "loki");
-if (res) {
-  mkdir(".OdinSnap");
-  await generateStats();
-} else {
-  console.warn(
-    "OdinSnap requires 'loki' to be installed as a Dependency for visual regression testing. Please install it to continue.",
-  );
-}
+await trimStats(
+  "./.OdinSnap/preview-stats.json",
+  "./.OdinSnap/trimmed-stats.json",
+);
+// if (res) {
+//   mkdir(".OdinSnap");
+//   await generateStats();
+// } else {
+//   console.warn(
+//     "OdinSnap requires 'loki' to be installed as a Dependency for visual regression testing. Please install it to continue.",
+//   );
+// }

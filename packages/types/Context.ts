65d15c13b521e14a89c0fb66bc0697038aaffbce
Anxhul10 <anxhul10@gmail.com>
2025-12-04 23:25:21 +0530

    chore: add exec dependency and change file git

---
+++ packages/types/Context.ts
@@ -0,0 +1,15 @@
+export interface Context {
+  payload: {
+    pull_request: head_base
+  }
+}
+
+interface head_base {
+  head: sha_ref;
+  base: sha_ref;
+}
+
+interface sha_ref {
+  sha: string;
+  ref: string;
+}

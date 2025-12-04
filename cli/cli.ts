df71cda626c348a9d2a7fa703cd5b01eb807c58d
Anxhul10 <anxhul10@gmail.com>
2025-12-04 20:58:19 +0530

    chore: add generate stats to binary

--- cli/cli.ts
+++ cli/cli.ts
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 import { checkPkgExist } from '../packages/cliHelpers/checkPkgExist.js';
+import { generateStats } from '../packages/cliHelpers/generateStats.js';
+import { mkdir } from '../packages/cliHelpers/mkdir.js';
 
 const res = checkPkgExist('./package.json', 'loki');
 if(res) {
-    console.log("loki exist");
+    mkdir('.OdinSnap');
+    await generateStats();
 }
 else {
     console.log('loki doesnt exist');

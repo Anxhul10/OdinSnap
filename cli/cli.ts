7be862cbefa6eeeb64cf26db2944ec8eb8abf3c0
Anxhul10 <anxhul10@gmail.com>
2026-01-08 00:11:24 +0530

    chore: check monorepo or not & wrap in runner function

--- cli/cli.ts
+++ cli/cli.ts
@@ -2,13 +2,28 @@
 import { checkPkgExist } from "@/packages/cliHelpers/checkPkgExist";
 import { generateStats } from "@/packages/cliHelpers/generateStats";
 import { mkdir } from "@/packages/cliHelpers/mkdir";
+import fs from "fs";
 
-const res = checkPkgExist("./package.json", "loki");
-if (res) {
-  mkdir(".OdinSnap");
-  await generateStats();
-} else {
-  console.warn(
-    "OdinSnap requires 'loki' to be installed as a Dependency for visual regression testing. Please install it to continue.",
-  );
+export async function runner() {
+  const res = checkPkgExist("./package.json", "loki");
+  if (res) {
+    mkdir(".OdinSnap");
+    await generateStats();
+
+    // checks project is monorepo or not
+    fs.stat("dependency-graph.json", function (err, _stat) {
+      if (err == null) {
+        console.log("Monorepo");
+      } else if (err.code === "ENOENT") {
+        // file does not exist
+        console.log("Not a Monorepo");
+      } else {
+        console.log("Some other error: ", err.code);
+      }
+    });
+  } else {
+    console.warn(
+      "OdinSnap requires 'loki' to be installed as a Dependency for visual regression testing. Please install it to continue.",
+    );
+  }
 }
